37910a073effa496943dab3330e0061dc70663cc
Nicknan <jurawankp@gmail.com>
2023-02-18 20:49:27 +0700

    1

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "databank";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+/*$link = mysqli_connect($servername, $username, $password, $db_name);
+if(!$link)
+echo "Connection failed";
+
+date_default_timezone_set("Asia/Bangkok");
+if(!$link->set_charset("utf8"))
+echo "Can't set charector UTF"*/
+
+?>
---
+++ save_creat_people.php
@@ -0,0 +1,17 @@
+<?php include 'connect.php'; ?>
+<?php
+    $id_card=$_POST["id_card"];
+    $titlename=$_POST["titlename"];
+    $fullname=$_POST["fullname"];
+    $phone=$_POST["phone"];
+    $email=$_POST["email"];
+    $birthday=$_POST["birthday"];
+    $age=$_POST["age"];
+    $account_name=$_POST["account_name"];
+    $mmoney=$_POST["mmoney"];
+    $aaddress=$_POST["aaddress"];
+    $ppassword=$_POST["ppassword"];
+    $email=$_POST["email"];
+    mysqli_query($conn, "INSERT INTO databank (id_card,titlename,fullname,phone,email,birthday,age,account_name,mmoney,aaddress,ppassword)
+    VALUES('$id_card','$titlename','$fullname','$phone','$email','$birthday','$age','$account_name','$mmoney','$aaddress','$ppassword')");
+?>
